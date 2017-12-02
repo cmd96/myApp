bf3da822932634c42db65bc01a4cf739e689129a
chemda <chemda96@gmail.com>
2017-12-02 19:30:32 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public name: string;
+  public lname: string;
+  private itemDoc: AngularFirestoreDocument<any>;
+
+
+  
+  constructor(private afs: AngularFirestore){
+    this.itemDoc = this.afs.doc('users/1');
+    let item = this.itemDoc.valueChanges().subscribe(res=>{
+      this.name = res.name
+    });
+  }
+
+  send(){
+    this.itemDoc.set({name: this.name, lname: this.lname}).then(res=>{
+    //  alert(res);
+    alert(this.name + ", " + this.lname);
+   })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBj9XabgD32Wi2qUrKm7AZo4ZjQErXFKRo",
+    authDomain: "tmcosm-48ed4.firebaseapp.com",
+    databaseURL: "https://tmcosm-48ed4.firebaseio.com",
+    projectId: "tmcosm-48ed4",
+    storageBucket: "tmcosm-48ed4.appspot.com",
+    messagingSenderId: "557253961976"
+  }
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
